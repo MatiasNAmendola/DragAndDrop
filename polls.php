48d922be1df96bbdbb8f896ff1fb4a68f949641b
gordonslondon <jules.boussekeyt@gmail.com>
2011-08-20 21:33:55 +0200

    updated polls order

--- polls.php
+++ polls.php
@@ -11,49 +11,49 @@ return array(
     array(
         'id' => 2,
         'name' => 'Poll 2',
-        'order' => 3,
+        'order' => 1,
         'visible' => false,
         'choices' => array('Choice 1', 'choice 2', 'Choice 3', 'choice 4')
     ),
     array(
         'id' => 3,
         'name' => 'Poll 3',
-        'order' => 1,
+        'order' => 2,
         'visible' => true,
         'choices' => array('Choice 1', 'Choice 2')
     ),
     array(
         'id' => 0,
         'name' => 'Poll 4',
-        'order' => 2,
+        'order' => 3,
         'visible' => false,
         'choices' => array('Choice 1', 'choice 2', 'Choice 3', 'choice 4')
     ),
     array(
         'id' => 6,
         'name' => 'Poll 5',
-        'order' => 6,
+        'order' => 4,
         'visible' => false,
         'choices' => array('Choice 1', 'choice 2', 'Choice 3')
     ),
     array(
         'id' => 7,
         'name' => 'Poll 6',
-        'order' => 7,
+        'order' => 5,
         'visible' => false,
         'choices' => array('Choice 1', 'choice 2', 'Choice 3', 'choice 4')
     ),
     array(
         'id' => 4,
         'name' => 'Poll 7',
-        'order' => 4,
+        'order' => 6,
         'visible' => false,
         'choices' => array('Choice 1', 'choice 2', 'Choice 3', 'choice 4', 'choice 5')
     ),
     array(
         'id' => 5,
         'name' => 'Poll 8',
-        'order' => 5,
+        'order' => 7,
         'visible' => false,
         'choices' => array('Choice 1', 'choice 2', 'Choice 3', 'choice 4')
     ),
